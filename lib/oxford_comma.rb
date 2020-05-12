bb7bb1333e1397c36f0dc89dbdff23346de42764
Morgan Collado <morgan.collado@gmail.com>
2020-05-12 00:20:39 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,10 @@
 def oxford_comma(array)
- if array = 1 
+ if array == 1 
    array.join
-  elsif array.join(" and ")
-  elsif  array.insert(2, "and")
+  elsif array == 2 
+  array.join(" and ")
+  elsif array == 3  
+  array.insert(2, "and")
     array.join(",")
   end
 end
