c86f83c2dbb51fc6d515861bc7112d9cb9122c1b
Morgan Collado <morgan.collado@gmail.com>
2020-05-12 00:41:32 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+ if array.size == 1
+   array.join
+  elsif array.size == 2 
+  array.join(" and ")
+  else  
+  array[-1].insert(0, "and ")
+  #last = array.pop 
+  #array << "and "
+    array.join(", ") #+ last
+  end
 end
